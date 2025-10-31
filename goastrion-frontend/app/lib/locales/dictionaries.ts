7bd2417a6b4982c87e4501cb33277b89bdb27105
sukumar <sukumarbarman@gmail.com>
2025-10-31 19:31:56 +0530

    language  commented

--- goastrion-frontend/app/lib/locales/dictionaries.ts
+++ goastrion-frontend/app/lib/locales/dictionaries.ts
@@ -2,14 +2,15 @@
 import en from "./en";
 import hi from "./hi";
 import bn from "./bn";
+/*
 import ta from "./ta";
 import te from "./te";
 import kn from "./kn";
 import es from "./es";
 import pt from "./pt";
 import fr from "./fr";
-
-export const dictionaries = { en, hi, bn, ta, te, kn, es, pt, fr } as const;
+*/
+export const dictionaries = { en, hi, bn,/*ta, te, kn, es, pt, fr */} as const;
 
 export type Dictionaries = typeof dictionaries;
 export type Locale = keyof Dictionaries; // "en" | "hi" | "bn" | "ta" | "te" | "kn" | "es" | "pt" | "fr"
